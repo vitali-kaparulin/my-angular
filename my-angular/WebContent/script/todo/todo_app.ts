dfc79f8c7bafd3e6e11f3ca546670fb4374de71a
Vitali Kaparulin <vitali.kaparulin@vika.fr>
2016-03-31 21:08:41 +0200

    todo app

---
+++ my-angular/WebContent/script/todo/todo_app.ts
@@ -0,0 +1,30 @@
+import {Component} from 'angular2/core';
+import {Todo} from './todo';
+import {TodoList} from './todo_list';
+import {TodoForm} from './todo_form';
+@Component({
+    selector: 'todo-app',
+    templateUrl: 'WebContent/html/todo/app.html',
+    styles: ['a { cursor: pointer; cursor: hand; }'],
+    directives: [TodoList, TodoForm]
+})
+export class TodoApp {
+    todos: Todo[] = [
+        { text: 'learn angular', done: true },
+        { text: 'build an angular app', done: false }
+    ];
+    get remaining() {
+        return this.todos.reduce((count: number, todo: Todo) => count + +!todo.done, 0);
+    }
+    archive(): void {
+        var oldTodos = this.todos;
+        this.todos = [];
+        oldTodos.forEach((todo: Todo) => {
+            if (!todo.done) this.todos.push(todo);
+        });
+    }
+    addTask(task: Todo) {
+        this.todos.push(task);
+    }
+}
+
---
+++ my-angular/WebContent/script/todo/todo_list.ts
@@ -0,0 +1,16 @@
+import {Component, Input} from 'angular2/core';
+import {Todo} from './todo';
+@Component({
+    selector: 'todo-list',
+    styles: [`
+    .done-true {
+      text-decoration: line-through;
+      color: grey;
+    }`
+    ],
+    templateUrl: 'WebContent/html/todo/list.html'
+})
+export class TodoList {
+    @Input() todos: Todo[];
+}
+
